4eeca9c6e25a936f4ff1e46ec72932b0a91feda8
D4nielF <31454925+D4nielF@users.noreply.github.com>
2017-08-30 10:05:37 +0200

    First Build1

---
+++ HelloWorld/HelloWorld_Build1/GlobalVars.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_Build1
+{
+   public class GlobalVars
+    {
+        private int _cnt;
+        
+
+        public string GetText
+        {
+            get
+            {
+                return "Hallo nochmal";
+            }
+        }
+
+        public void SetCnt()
+        {
+            _cnt++;
+            if (_cnt >= 2)
+            {
+                _cnt = 0;
+            }
+        }
+
+    }
+}
